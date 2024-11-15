128ddc5f5ade5a80aa2abf1825cfca88a25be6d4
veronicamagnanimi <veronica.magnanimi@hotmail.it>
2024-11-15 14:27:19 +0100

    feat: snack 1 complete

--- snack-1/script.js
+++ snack-1/script.js
@@ -2,3 +2,6 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+names.forEach((curNames) => 
+console.log(curNames));
